a9ec9c5b25c3dc913d9585caff7e573a7031cfb2
Bruno Mogetta <brunomogetta@gmail.com>
2023-03-28 17:01:54 -0300

    feat: added throw if env is not defined

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,8 +1,11 @@
-import { dotenv } from 'deps';
+import { dotenv } from "deps";
 
 await dotenv.load({ export: true, allowEmptyValues: true });
 
-const COINMARKETCAP_API_KEY = Deno.env.get('COINMARKETCAP_API_KEY');
-const ALCHEMY_API_KEY = Deno.env.get('ALCHEMY_API_KEY');
+const COINMARKETCAP_API_KEY = Deno.env.get("COINMARKETCAP_API_KEY");
+const ALCHEMY_API_KEY = Deno.env.get("ALCHEMY_API_KEY");
+
+if (COINMARKETCAP_API_KEY || COINMARKETCAP_API_KEY)
+  throw new Error("Environment variables are not define");
 
 export { ALCHEMY_API_KEY, COINMARKETCAP_API_KEY };
